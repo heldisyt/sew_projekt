c3402fd0fd3a938fcfcc908a61baa4b19199c1ea
heldi valikaj <heldivalikajbsn@gmail.com>
2020-02-27 09:50:37 +0100

    update

--- helvel_sew_projekt2/src/helvel_sew_projekt2/Helvel_sew_projekt2.java
+++ helvel_sew_projekt2/src/helvel_sew_projekt2/Helvel_sew_projekt2.java
@@ -15,7 +15,9 @@ public class Helvel_sew_projekt2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
     }
     
 }
